86c9646675f076bbd776039d30685d192520a4a5
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-09-13 13:08:04 -0400

    Carga de BUsqueda

--- pie.php
+++ pie.php
@@ -15,7 +15,7 @@
     <script src="<?php echo $folder;?>js/core/bootstrap.min.js"></script>
     <script src="<?php echo $folder;?>js/core/plugins/jquery.slimscroll.min.js"></script>
     <script src="<?php echo $folder;?>js/core/plugins/jquery.form.js"></script>
-
+    <script src="<?php echo $folder;?>js/busqueda.js" type="text/javascript" language="javascript"></script>
     <!--Beyond Scripts-->
     <script src="<?php echo $folder;?>js/core/assets/beyond.min.js"></script>
 
