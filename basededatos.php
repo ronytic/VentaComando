a5f5e843be2e5413e4f90e7ef7384a56f87a8034
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-09-13 12:13:27 -0400

    BD

--- basededatos.php
+++ basededatos.php
@@ -45,5 +45,54 @@ function consulta($sql){
         return $resultado;
     }
 }
-
+function insertarRegistro($nombretabla,$Values,$Todo=1,$sw=1){
+		if($Todo==1){
+			$fecha=date("Y-m-d");
+			$hora=date("H:i:s");
+			$CodUsuario=$_SESSION['CodUsuarioLog'];
+			$Nivel=$_SESSION['Nivel'];
+			
+			if(!isset($Values['nivel'])&& empty($Values['Nivel'])){
+                $Values['nivel']="$Nivel";
+                if($Values['nivel']==""){
+                    $Values['nivel']=1;    
+                }
+			}
+			if(!isset($Values['fecharegistro'])&& empty($Values['fecharegistro'])){
+				$Values['fecharegistro']="'$fecha'";
+			}
+			if(!isset($Values['horaregistro'])&& empty($Values['horaregistro'])){
+				$Values['horaregistro']="'$hora'";
+			}
+			if(!isset($Values['codusuario'])&& empty($Values['codusuario'])){
+				$Values['codusuario']="$CodUsuario";
+                 if($Values['codusuario']==""){
+                    $Values['codusuario']=1;    
+                }
+			}
+			$Values['activo']=1;
+		}else{//,array("Activo"=>1)
+			$Values=array_merge	($Values);	
+		}
+		$data=$Values;
+		//print_r($Values);
+		$key=array();
+		$val=array();
+		foreach($data as $k => $v){
+			$key[]=$k;
+			$val[]=$v;
+		}
+		$campos=implode(",",$key);
+		$datos= implode(",",$val);
+		////
+		$nombretabla=mb_strtolower($nombretabla,"utf8");
+		if($sw==0)
+			$query ="INSERT INTO {$nombretabla} VALUES ($datos)";
+		else
+			$query ="INSERT INTO {$nombretabla} ($campos) VALUES ($datos)";
+			
+		echo $query."<br>";
+		//echo "NO ESTA HABILITADO EL REGISTRO";
+        consulta($query);
+	}
 ?>
