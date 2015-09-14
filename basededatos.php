087aae843779e3883443e62a3dae45c7d2960d33
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-09-13 20:17:39 -0400

    BD

--- basededatos.php
+++ basededatos.php
@@ -46,53 +46,77 @@ function consulta($sql){
     }
 }
 function insertarRegistro($nombretabla,$Values,$Todo=1,$sw=1){
-		if($Todo==1){
-			$fecha=date("Y-m-d");
-			$hora=date("H:i:s");
-			$CodUsuario=$_SESSION['CodUsuarioLog'];
-			$Nivel=$_SESSION['Nivel'];
-			
-			if(!isset($Values['nivel'])&& empty($Values['Nivel'])){
-                $Values['nivel']="$Nivel";
-                if($Values['nivel']==""){
-                    $Values['nivel']=1;    
-                }
-			}
-			if(!isset($Values['fecharegistro'])&& empty($Values['fecharegistro'])){
-				$Values['fecharegistro']="'$fecha'";
-			}
-			if(!isset($Values['horaregistro'])&& empty($Values['horaregistro'])){
-				$Values['horaregistro']="'$hora'";
-			}
-			if(!isset($Values['codusuario'])&& empty($Values['codusuario'])){
-				$Values['codusuario']="$CodUsuario";
-                 if($Values['codusuario']==""){
-                    $Values['codusuario']=1;    
-                }
-			}
-			$Values['activo']=1;
-		}else{//,array("Activo"=>1)
-			$Values=array_merge	($Values);	
-		}
-		$data=$Values;
-		//print_r($Values);
-		$key=array();
-		$val=array();
-		foreach($data as $k => $v){
-			$key[]=$k;
-			$val[]=$v;
-		}
-		$campos=implode(",",$key);
-		$datos= implode(",",$val);
-		////
-		$nombretabla=mb_strtolower($nombretabla,"utf8");
-		if($sw==0)
-			$query ="INSERT INTO {$nombretabla} VALUES ($datos)";
-		else
-			$query ="INSERT INTO {$nombretabla} ($campos) VALUES ($datos)";
-			
-		echo $query."<br>";
-		//echo "NO ESTA HABILITADO EL REGISTRO";
-        consulta($query);
-	}
+    if($Todo==1){
+        $fecha=date("Y-m-d");
+        $hora=date("H:i:s");
+        $CodUsuario=$_SESSION['CodUsuarioLog'];
+        $Nivel=$_SESSION['Nivel'];
+        
+        if(!isset($Values['nivel'])&& empty($Values['Nivel'])){
+            $Values['nivel']="$Nivel";
+            if($Values['nivel']==""){
+                $Values['nivel']=1;    
+            }
+        }
+        if(!isset($Values['fecharegistro'])&& empty($Values['fecharegistro'])){
+            $Values['fecharegistro']="'$fecha'";
+        }
+        if(!isset($Values['horaregistro'])&& empty($Values['horaregistro'])){
+            $Values['horaregistro']="'$hora'";
+        }
+        if(!isset($Values['codusuario'])&& empty($Values['codusuario'])){
+            $Values['codusuario']="$CodUsuario";
+             if($Values['codusuario']==""){
+                $Values['codusuario']=1;    
+            }
+        }
+        $Values['activo']=1;
+    }else{//,array("Activo"=>1)
+        $Values=array_merge	($Values);	
+    }
+    $data=$Values;
+    //print_r($Values);
+    $key=array();
+    $val=array();
+    foreach($data as $k => $v){
+        $key[]=$k;
+        $val[]=$v;
+    }
+    $campos=implode(",",$key);
+    $datos= implode(",",$val);
+    ////
+    $nombretabla=mb_strtolower($nombretabla,"utf8");
+    if($sw==0)
+        $query ="INSERT INTO {$nombretabla} VALUES ($datos)";
+    else
+        $query ="INSERT INTO {$nombretabla} ($campos) VALUES ($datos)";
+        
+    echo $query."<br>";
+    //echo "NO ESTA HABILITADO EL REGISTRO";
+    //consulta($query);
+}
+function actualizarRegistro($nombretabla,$Values,$cod){
+    $data=$Values;
+    //print_r($Values);
+    $key=array();
+    $val=array();
+    foreach($data as $k => $v){
+        $key[]=$k."=".$v."";
+        $val[]=$v;
+    }
+    $campos=implode(",",$key);
+    $datos= implode(",",$val);
+    ////
+    $nombretabla=mb_strtolower($nombretabla,"utf8");
+    $query ="UPDATE {$nombretabla} SET $campos WHERE cod$nombretabla=".$cod;
+        
+    //echo $query."<br>";
+    //echo "NO ESTA HABILITADO EL REGISTRO";
+    consulta($query);
+}
+function eliminar($nombretabla,$cod){
+    $sql="UPDATE $nombretabla SET activo=0 WHERE cod$nombretabla=".$cod;
+    //echo $sql;
+    consulta($sql);
+}
 ?>
