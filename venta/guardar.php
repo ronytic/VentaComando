40c5d26bf7ef45fe24039f09e5f9f06c4a78b2b7
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-10-07 23:34:46 -0400

    Venta Guardar

--- venta/guardar.php
+++ venta/guardar.php
@@ -1,28 +1,94 @@
 <?php
 $folder="../";
 include_once("../basededatos.php");
-extract($_POST);
-$valores=array("total"=>"'$total'",
-                "nombre"=>"'$nombre'",
-                "ci"=>"'$ci'",
-                );
+if(!empty($_POST)){
+    extract($_POST);
+    $valores=array("total"=>"'$total'",
+                    "nombre"=>"'$nombre'",
+                    "ci"=>"'$ci'",
+                    );
 
-insertarRegistro("venta",$valores);
-$codventa=ultimo();
-$sql="SELECT * FROM detalleventatemporal ";
-$reg=consulta($sql);
- foreach($reg as $r){$i++;
-$valores=array("codventa"=>"'$codventa'",
-                "codproducto"=>"'".$r['codproducto']."'",
-                "cantidad"=>"'".$r['cantidad']."'",
-                "precioventa"=>"'".$r['precioventa']."'",
-                "total"=>"'".$r['total']."'",
-                );
+    insertarRegistro("venta",$valores);
+    $codventa=ultimo();
+    $sql="SELECT * FROM detalleventatemporal ";
+    $reg=consulta($sql);
 
-insertarRegistro("ventadetalle",$valores);
-$sql="DELETE FROM detalleventatemporal WHERE coddetalleventatemporal=".$r['coddetalleventatemporal'];
-consulta($sql);
- }
+    foreach($reg as $r){$i++;
+        /*Desde Aqui*/
+
+        if($r['codproducto']==""){
+            continue;   
+        }
+        $codproductos=$r['codproducto'];
+        $cantidad=$r['cantidad'];
+        $cantidadventatotal=$prod['cantidad'];
+        $preciounitario=$prod['precioventa'];
+        $subtotal=$prod['total'];
+        
+        $fecha=date("Y-m-d");
+        $totalproducto=0;
+
+        $sql="SELECT sum(cantidadstock) as cantidastock FROM compra WHERE codproducto LIKE '".$r['codproducto']."' and activo=1";
+        $cantidastock=consulta($sql);
+        $cantidastock=array_shift($cantidastock);
+        $stock=$cantidastock['cantidastock'];
+        if($stock==""){$stock=0;}
+        $totalproducto=$stock;
+
+        //echo $totalproducto."<br>";
+        //exit();
+        //echo $totalproducto;
+        $sql="SELECT * FROM producto WHERE codproducto=".$r['codproducto'];
+        $re=consulta($sql);
+        $pr=array_shift($re);
+        $nombreproducto=$pr['nombre'];
+        
+        if($totalproducto<$cantidad){
+            $mensaje[]="No Existe en Inventario la Cantidad que Solicita<hr><strong><br>Nombre Producto: $nombreproducto<br>Cantidad de Inventario: $totalproducto<br>Cantidad de Solicitada: $cantidad</strong>";
+        }else{
+            $sql="SELECT * FROM compra WHERE codproducto=$codproductos and cantidadstock>0 ORDER BY fechacompra";
+        
+            $com=consulta($sql);
+            foreach($com as $inv){
+                if((float)$cantidad<=(float)$inv['cantidadstock']){
+                    //echo "si";
+                    $mensaje[]="La Venta del producto ".mb_strtoupper($nombreproducto,"utf8")." se registro correctamente";
+                    
+                    $cantidad=$inv['cantidadstock']-$cantidad;
+                    $valores=array("cantidadstock"=>"$cantidad");
+                    actualizarRegistro("compra",$valores,$inv["codcompra"]);
+                    
+                    //print_r($valores);
+
+                    $valores=array("codventa"=>"'$codventa'",
+                        "codproducto"=>"'".$r['codproducto']."'",
+                        "cantidad"=>"'".$r['cantidad']."'",
+                        "precioventa"=>"'".$r['precioventa']."'",
+                        "total"=>"'".$r['total']."'",
+                        );
+
+                    insertarRegistro("ventadetalle",$valores);
+        
+                    break;  
+                }else{
+                    //echo $cantidadsalida;
+                    $cantidad=$cantidad-$inv['cantidadstock'];
+                    $valores=array("cantidadstock"=>0);
+                    actualizarRegistro("compra",$valores,$inv["codcompra"]);
+                }
+            }
+        }
+
+
+
+
+
+        /*HAsta Aqui*/
+        
+        $sql="DELETE FROM detalleventatemporal WHERE coddetalleventatemporal=".$r['coddetalleventatemporal'];
+        consulta($sql);
+    }
+}
 include_once($folder."cabecerahtml.php");
 ?>
 
@@ -34,7 +100,7 @@ include_once($folder."cabecerahtml.php");
         <strong>Venta Realizada Correctamente</strong>
         </div>
         <a href="../index.php" class="btn btn-blue">Nueva Venta</a>
-        <a href="boleta.php?codventa=<?php echo $codventa?>" class="btn btn-success">Boleta</a>
+        <!--<a href="boleta.php?codventa=<?php echo $codventa?>" class="btn btn-success">Boleta</a>-->
     </div>
    
 </div>
